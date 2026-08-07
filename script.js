c9c55dba531e86677ec121f66d1c6cf2734d5a36
firstek22-ux <firstek22@gmail.com>
2026-08-07 22:37:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,49 @@ document
 
 
 }
+
+
+
+function openLogin(){
+
+document.getElementById("loginBox").style.display="block";
+
+}
+
+
+function closeLogin(){
+
+document.getElementById("loginBox").style.display="none";
+
+}
+
+
+
+function login(){
+
+let user=document.getElementById("user").value;
+
+let pass=document.getElementById("pass").value;
+
+
+// 測試帳號
+
+if(user=="admin" && pass=="123456"){
+
+alert("身份確認成功");
+
+document.getElementById("loginBox").style.display="none";
+
+
+document.getElementById("postArea").style.display="block";
+
+
+}
+
+else{
+
+alert("身份驗證失敗");
+
+}
+
+}
